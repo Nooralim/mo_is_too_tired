0f20785cbfcccea48484bae5309cb2fddbe42ebe
nooralim <muhib.noorali2@gmail.com>
2016-11-30 19:36:58 -0800

    cleaned up view and display of app

---
+++ app/src/main/java/com/bignerdranch/android/criminalintent/Crime.java
@@ -0,0 +1,40 @@
+package com.bignerdranch.android.criminalintent;
+
+import android.widget.ImageView;
+
+import java.util.Date;
+import java.util.UUID;
+
+
+public class Crime {
+
+    private UUID mId;
+    private String mTitle;
+
+
+    public ImageView getImage() {
+        return image;
+    }
+
+    public void setImage(ImageView image) {
+        this.image = image;
+    }
+
+    private ImageView image;
+
+    public Crime() {
+        mId = UUID.randomUUID();
+    }
+
+    public UUID getId() {
+        return mId;
+    }
+
+    public String getTitle() {
+        return mTitle;
+    }
+
+    public void setTitle(String title) {
+        mTitle = title;
+    }
+}
---
+++ app/src/main/java/com/bignerdranch/android/criminalintent/CrimeLab.java
@@ -0,0 +1,51 @@
+package com.bignerdranch.android.criminalintent;
+
+import android.content.Context;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+import android.widget.ImageView;
+
+public class CrimeLab {
+    private static CrimeLab sCrimeLab;
+
+    private ArrayList<Crime> mCrimes;
+
+    public static CrimeLab get(Context context) {
+        if (sCrimeLab == null) {
+            sCrimeLab = new CrimeLab(context);
+        }
+        return sCrimeLab;
+    }
+
+    private CrimeLab(Context context) {
+        mCrimes = new ArrayList<>();
+        for (int i = 0; i < 100; i++) {
+            Crime crime = new Crime();
+            crime.setTitle("Picture #" + i);
+            ImageView v1 = new ImageView(context);
+
+            crime.setImage(v1);
+            v1.setImageResource(context.getResources().getIdentifier("drawable/images" + (i%3), null, context.getPackageName()));
+
+            mCrimes.add(crime);
+
+
+
+        }
+    }
+
+    public List<Crime> getCrimes() {
+        return mCrimes;
+    }
+
+    public Crime getCrime(UUID id) {
+        for (Crime crime : mCrimes) {
+            if (crime.getId().equals(id)) {
+                return crime;
+            }
+        }
+        return null;
+    }
+}
